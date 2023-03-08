f6276a23895e655e9438bc8df3f19926fbffe036
GentMaker <daohung.rks@gmail.com>
2023-03-08 17:42:54 +0700

    feature/#602/reset_password

--- src/main/java/ProjectBlogOJT/controller/UserController.java
+++ src/main/java/ProjectBlogOJT/controller/UserController.java
@@ -6,6 +6,7 @@ import ProjectBlogOJT.model.entity.Roles;
 import ProjectBlogOJT.model.entity.User;
 import ProjectBlogOJT.model.service.RoleService;
 import ProjectBlogOJT.model.service.UserSevice;
+import ProjectBlogOJT.payload.request.ChangePass;
 import ProjectBlogOJT.payload.request.LoginRequest;
 import ProjectBlogOJT.payload.request.SignupRequest;
 import ProjectBlogOJT.payload.response.JwtResponse;
@@ -18,9 +19,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.PermitAll;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -61,6 +64,25 @@ public class UserController {
         user.setUserPassword(encoder.encode(newPass));
 
         return userSevice.saveOrUpdate(user);
+    }
+    @PutMapping("/changePass")
+    public ResponseEntity<?> changePassword(@RequestBody ChangePass changePass) {
+        CustomUserDetails userDetails = (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User users = userSevice.findByID(userDetails.getUserId());
+        BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+        boolean passChecker = bc.matches(changePass.getOldPassword(), users.getUserPassword());
+        if (passChecker) {
+            boolean checkDuplicate = bc.matches(changePass.getPassword(), users.getUserPassword());
+            if (checkDuplicate) {
+                return ResponseEntity.ok(new MessageResponse("The new password must be different from the old password !"));
+            } else {
+                users.setUserPassword(encoder.encode(changePass.getPassword()));
+                userSevice.saveOrUpdate(users);
+                return ResponseEntity.ok(new MessageResponse("Change password successfully !"));
+            }
+        } else {
+            return ResponseEntity.ok(new MessageResponse("Password does not match ! Change password fail"));
+        }
     }
 
 
@@ -115,7 +137,7 @@ public class UserController {
         );
         SecurityContextHolder.getContext().setAuthentication(authentication);
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-        User users = userSevice.findByEmail(customUserDetails.getUserEmail());
+        User users = userSevice.findByEmail(customUserDetails.getEmail());
         if(!customUserDetails.isUserStatus()){
             return ResponseEntity.ok("Your account have been block !");
         } else {
